8dd6796ecc900b34e0f8f523e721f4be11df375f
Ulaş Erkuş <ulas.erkus@omerd.com>
2022-12-24 00:52:39 +0300

    routing an components

---
+++ BlazorApp/Models/DropDownProps.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class DropDownProps
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
