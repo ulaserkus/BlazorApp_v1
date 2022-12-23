b4cdd4513acb68cee2229d398849320af2b6df6a
Ulaş Erkuş <ulas.erkus@omerd.com>
2022-12-24 00:20:00 +0300

    One-Two way binding

---
+++ BlazorApp/Models/CourseTraining.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Models
+{
+    public class CourseTraining
+    {
+        public int Id { get; set; }
+
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
